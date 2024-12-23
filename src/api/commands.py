f8c706f4ed3233543e868436c33612881f97f726
Alejandro <64385399+Fractal-Vortex@users.noreply.github.com>
2024-12-23 12:54:40 +0000

    added comentarios y valoraciones

--- src/api/commands.py
+++ src/api/commands.py
@@ -8,7 +8,7 @@ Flask commands are usefull to run cronjobs or tasks outside of the API but sill 
 with youy database, for example: Import the price of bitcoin every night as 12am
 """
 def setup_commands(app):
-    
+
     """ 
     This is an example command "insert-test-users" that you can run from the command line
     by typing: $ flask insert-test-users 5
@@ -32,3 +32,4 @@ def setup_commands(app):
     @app.cli.command("insert-test-data")
     def insert_test_data():
         pass
+
--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, datetime
 from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 
@@ -19,7 +19,7 @@ class Users(db.Model):
             "id": self.id,
             "email": self.email,
         }
-    
+
 class Rutas(db.Model):
     __tablename__ = 'rutas'
     id = db.Column(db.Integer, primary_key=True)
@@ -29,7 +29,6 @@ class Rutas(db.Model):
     category_id = db.Column(db.Integer, ForeignKey("categorias.id"), nullable=True)
     fecha_creada = db.Column(db.Date, nullable=False)
     fecha_inicio = db.Column(db.Date, nullable=False)
-
 
     user = relationship('Users', foreign_keys=[usuario_id], backref="rutas")
     categoria = relationship('Categorias', foreign_keys=[category_id], backref="rutas")
@@ -61,3 +60,119 @@ class Categorias(db.Model):
             "id": self.id,
             "categoria": self.categoria,
         }
+
+class Eventos(db.Model):
+    __tablename__ = 'eventos'
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(120), unique=True, nullable=False)
+    detalles = db.Column(db.JSON, nullable=True)
+    tipo = db.Column(db.String(120), nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, ForeignKey("categorias.id"), nullable=False)
+    rutas_id = db.Column(db.Integer, ForeignKey("rutas.id"), nullable=False)
+
+    categoria = relationship('Categorias', backref="eventos")
+    ruta = relationship('Rutas', backref="eventos")
+
+    def __repr__(self):
+        return f'<Eventos {self.titulo}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "titulo": self.titulo,
+            "detalles": self.detalles,
+            "tipo": self.tipo,
+            "fecha": self.fecha.isoformat() if self.fecha else None,
+            "category_id": self.category_id,
+            "rutas_id": self.rutas_id
+        }
+
+class Rutas_eventos(db.Model):
+    __tablename__ = 'rutas_eventos'
+    id = db.Column(db.Integer, primary_key=True)
+    ruta_id = db.Column(db.Integer, ForeignKey("rutas.id"), nullable=False)
+    evento_id = db.Column(db.Integer, ForeignKey("eventos.id"), nullable=False)
+
+    ruta = relationship("Rutas", backref="rutas_eventos")
+    evento = relationship("Eventos", backref="rutas_eventos")
+
+    def __repr__(self):
+        return f'<Rutas_eventos {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "ruta_id": self.ruta_id,
+            "evento_id": self.evento_id
+        }
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey("users.id"), nullable=True)
+    rutas_id = db.Column(db.Integer, ForeignKey("rutas.id"), nullable=True)
+    eventos_id = db.Column(db.Integer, ForeignKey("eventos.id"), nullable=True)
+
+    ruta = relationship("Rutas", backref="favorites")
+    evento = relationship("Eventos", backref="favorites")
+
+    def __repr__(self):
+        return f'<Favorites {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "rutas_id": self.rutas_id,
+            "eventos_id": self.eventos_id
+        }
+
+class Comentarios(db.Model):
+    __tablename__ = 'comentarios'
+    id = db.Column(db.Integer, primary_key=True)
+    comentario = db.Column(db.Text, nullable=True)
+    fecha_creacion = db.Column(db.DateTime, nullable=False)
+    tipo = db.Column(db.String(120), nullable=False)
+    from_id = Column(Integer, ForeignKey('users.id'))
+    to_id = Column(Integer, ForeignKey('users.id'))
+
+    from_user = relationship('Users', foreign_keys=[from_id], backref="comentarios_realizados")
+    to_user = relationship('Users', foreign_keys=[to_id], backref="comentarios_recibidos")
+
+    def __repr__(self):
+        return f'<Comentarios {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comentario": self.comentario,
+            "fecha_creacion": self.fecha_creacion.isoformat() if self.fecha_creacion else None,
+            "tipo": self.tipo,
+            "from_id": self.from_id,
+            "to_id": self.to_id,
+        }
+
+class Valoraciones(db.Model):
+    __tablename__ = 'valoraciones'
+    id = db.Column(db.Integer, primary_key=True)
+    valoracion = db.Column(db.Float, nullable=True)
+    tipo = db.Column(db.String(120), nullable=False)
+    from_id = Column(Integer, ForeignKey('users.id'))
+    to_id = Column(Integer, ForeignKey('users.id'))
+
+    from_user = relationship('Users', foreign_keys=[from_id], backref="valoraciones_realizados")
+    to_user = relationship('Users', foreign_keys=[to_id], backref="valoraciones_recibidos")
+
+    def __repr__(self):
+        return f'<Valoraciones {self.id}>'
+
+   def serialize(self):
+        return {
+            "id": self.id,
+            "valoracion": self.valoracion,
+            "tipo": self.tipo,
+            "from_id": self.from_id,
+            "to_id": self.to_id,
+        }
+
